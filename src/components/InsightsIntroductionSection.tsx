b163cf05488acff995dde132bc7e877c9c96a8b9
hicham <hicham.dbira10@gmail.com>
2024-12-02 12:12:20 +0100

    add responsiveness for InsightsIntroductionSection

--- src/components/InsightsIntroductionSection.tsx
+++ src/components/InsightsIntroductionSection.tsx
@@ -46,37 +46,37 @@ function InsightsIntroductionSection ()  {
       {/* Content */}
       <div className="relative z-10 flex flex-col md:flex-row items-center max-w-7xl mx-auto h-full  ">
       {/* Left Content */}
-      <div className="text-white w-full md:w-1/2 space-y-14 p-8 mt-32 md:mt-0">
-        
-          <span className="text-sky-500 text-5xl font-bold">Introducing:  <InsightsIcon style={{ fontSize: '3.5rem' }}/> +Insights</span>
-        
-          <ul className="space-y-6 pb-20">
-                {features.map((feature, index) => (
-                <li
-                key={index}
-                className="flex items-center space-x-8" // Use flex and items-center for vertical alignment
-                >
-                <img
-                    src={itemListIconWhite}
-                    width="28px"
-                    height="28px"
-                    className="flex-shrink-0" // Prevent the image from shrinking
-                />
-                <p className="text-white font-normal text-md sm:text-2xl">
-                   
-                    {feature}
-                    
-                </p>
-                </li>
-            ))}
-        </ul>
-      </div>
+      <div className="text-white w-full md:w-1/2 space-y-8 md:space-y-10 lg:space-y-14 px-4 md:px-6 lg:px-8 mt-32 md:mt-0">
+      <span className="text-sky-500 text-3xl sm:text-4xl lg:text-5xl font-bold flex items-center space-x-2">
+        Introducing: <InsightsIcon style={{ fontSize: '2.5rem' }} />
+        <span>+Insights</span>
+      </span>
+      <ul className="space-y-4 md:space-y-6 lg:space-y-8 pb-10 md:pb-14">
+        {features.map((feature, index) => (
+          <li
+            key={index}
+            className="flex items-center space-x-4 md:space-x-6 lg:space-x-8"
+          >
+            <img
+              src={itemListIconWhite}
+              width="24px"
+              height="24px"
+              className="flex-shrink-0 w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7"
+              alt="Icon"
+            />
+            <p className="text-white font-normal text-md md:text-lg lg:text-xl">
+              {feature}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
 
 
       {/* Right Content: two images - Centered and Responsive */}
       <div className="flex mb-8 mt-10 relative">
         <motion.div
-          className="relative -right-0 transform shadow-2xl rounded-full sm:-right-40 md:-right-50 lg:-right-50"
+          className="relative -right-0 transform shadow-2xl rounded-full sm:-right-20 md:-right-40 lg:-right-48 w-2/3  sm:w-1/2 md:w-[60%] lg:w-[80%]"
           initial={{ opacity: 0, x: 50 }}
           whileInView={{ opacity: 1, x: 0.3 }}
           viewport={{ once: true }}
@@ -85,9 +85,9 @@ function InsightsIntroductionSection ()  {
           <img src={instrumentScreen2} />
         </motion.div>
         <motion.div
-          className="absolute w-full -right-0 sm:-right-60 md:-right-80 lg:-right-80 md:w-auto shadow-2xl rounded-full"
+          className="absolute w-full shadow-2xl rounded-full right-16 sm:-right-20 md:-right-44 lg:-right-72     w-2/3 sm:w-1/2 md:w-[60%] lg:w-[80%]"
           initial={{ opacity: 0, x: -50 }}
-          whileInView={{ opacity: 1, x: 0.3, rotate: ['0deg', '20deg'] }}
+          whileInView={{ opacity: 1, x: 0.3, rotate: ['0deg', '14deg'] }}
           viewport={{ once: true }}
           transition={{ duration: 1.8 }}
         >
