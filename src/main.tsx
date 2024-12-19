a5567ae49a1c2f7d1132a71de5aaec65a5fe0c46
hicham <hicham.dbira10@gmail.com>
2024-12-19 13:25:45 +0100

    fix routing problm

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider  } from 'react-router-dom'
 import './index.css'
 import "./i18n";
 import Home from './pages/Home.tsx'
@@ -12,45 +12,61 @@ import Commodities from './pages/Commodities.tsx';
 import Shares from './pages/Shares.tsx';
 import Options from './pages/Options.tsx';
 import ETFS from './pages/ETFs.tsx';
-const router = createBrowserRouter([
+
+
+
+const router = createBrowserRouter(
+  [
+    { path: "/",
+      element: <Home />
+    },
+    { path: "*", 
+      element: <NotFoundPage /> }, 
+    {
+      path:'/crypto',
+      element: <Crypto/>,
+    },
+    {
+      path:'/indices',
+      element: <Indices/>,
+    },
+    {
+      path:'/forex',
+      element: <Forex/>,
+    },
+    {
+      path:'/commodities',
+      element: <Commodities/>,
+    },
+    {
+      path:'/shares',
+      element: <Shares/>,
+    },
+    {
+      path:'/options',
+      element: <Options/>,
+    },
+    {
+      path:'/etfs',
+      element: <ETFS/>,
+    }
+  ],
   {
-  path:'/',
-  element: <Home/>,
-  errorElement: <NotFoundPage/>
-  },
-  {
-    path:'/crypto',
-    element: <Crypto/>,
-  },
-  {
-    path:'/indices',
-    element: <Indices/>,
-  },
-  {
-    path:'/forex',
-    element: <Forex/>,
-  },
-  {
-    path:'/commodities',
-    element: <Commodities/>,
-  },
-  {
-    path:'/shares',
-    element: <Shares/>,
-  },
-  {
-    path:'/options',
-    element: <Options/>,
-  },
-  {
-    path:'/etfs',
-    element: <ETFS/>,
+    future: {
+      v7_relativeSplatPath: true, // Enables relative paths in nested routes
+      v7_fetcherPersist: true,   // Retains fetcher state during navigation
+      v7_normalizeFormMethod: true, // Normalizes form methods (e.g., POST or GET)
+      v7_partialHydration: true, // Supports partial hydration for server-side rendering
+      v7_skipActionErrorRevalidation: true, // Prevents revalidation when action errors occur
+    },
   }
-]);
+);
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider 
+     
+      router={router}/>
   </StrictMode>,
 )
